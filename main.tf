b514a81e2a3a73ddfb646cf08dc8e0675eccee07
Cap'n Duck <42878045+CapnDucks@users.noreply.github.com>
2022-07-15 16:02:18 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   rule {
     apply_server_side_encryption_by_default {
       kms_master_key_id = module.kms.kms_key.arn
-      sse_algorithm     = "AES256"
+      sse_algorithm     = "aws:kms"
     }
   }
 }
