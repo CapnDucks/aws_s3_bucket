2626d9ee0f3ad0415fa08c37d9aac9dec3052228
Cap'n Duck <coder@capnduck.com>
2023-05-09 12:20:49 -0700

    Fixing the S3 ACL's after AWS update

--- main.tf
+++ main.tf
@@ -9,11 +9,6 @@ resource "aws_s3_bucket" "this" {
   force_destroy = var.force_destroy
 }
 
-resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
-  acl    = var.s3_bucket_acl
-}
-
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.this.bucket
 
