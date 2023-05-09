767b033c002b19136aaaae021fbf4f9b8357a6dd
Cap'n Duck <42878045+CapnDucks@users.noreply.github.com>
2023-05-09 12:34:25 -0700

    Feature/tags (#3)
    * Tags
    
    * Tags
    
    ---------
    
    Co-authored-by: Cap'n Duck <coder@capnduck.com>

--- example/main.tf
+++ example/main.tf
@@ -1,8 +1,8 @@
 module "s3_bucket" {
+  #source = "git::https://github/com/CapnDucks/s3_bucket?ref=1.latest"
   source = "../"
 
   bucket        = "my-globally-unique-named-bucket"
   force_destroy = true
   kms_key_alias = "mykmsalias"
-  #s3_bucket_acl = "private"
 }
