e2f26582f3f54b13e42993efc93efe8a6a598af3
J Elliott <75694124+jelliottio@users.noreply.github.com>
2024-03-01 20:18:14 -0800

    Feature/add tags (#5)
    * Add tags
    
    * Updating KMS module.
    
    ---------
    
    Co-authored-by: Cap'n Duck <coder@capnduck.com>

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "kms" {
-  source = "git::https://github.com/CapnDucks/aws_kms?ref=v1.1"
+  source = "git::https://github.com/CapnDucks/aws_kms?ref=v1"
 
   kms_key_alias = var.kms_key_alias
 }
